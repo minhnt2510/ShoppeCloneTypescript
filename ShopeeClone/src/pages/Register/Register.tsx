56871ae3bd36ce00bf8d71d0da2915112cdd86df
MinhNguyen2k4 <tanminh.office183@gmail.com>
2025-07-07 12:25:32 +0700

    form login/register

--- ShopeeClone/src/pages/Register/Register.tsx
+++ ShopeeClone/src/pages/Register/Register.tsx
@@ -1,5 +1,66 @@
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div>dk</div>;
+  return (
+    <div className="bg-orange-600">
+      {/* container */}
+      <div className="max-w-7xl mx-auto px-4">
+        {/* //form  */}
+        <div className="grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10">
+          <div className="lg:col-span-2 lg:col-start-4">
+            <form className="p-10 rounded bg-white shadow-sm">
+              <div className="text-2xl">Đăng ký</div>
+              <div className="mt-8">
+                <input
+                  type="email"
+                  name="email"
+                  className="p-3 w-full outline-none border border-gray-400 focus:border-gray-600 rounded-sm
+                  focus:shadow-sm"
+                  placeholder="Email"
+                />
+                <div className="mt-1 text-red-500 min-h-[1rem] text-sm">
+                  Email không hợp lệ
+                </div>
+              </div>
+              <div className="mt-3">
+                <input
+                  type="password"
+                  name="password"
+                  className="p-3 w-full outline-none border border-gray-400 focus:border-gray-600 rounded-sm
+                  focus:shadow-sm"
+                  placeholder="Password"
+                />
+                <div className="mt-1 text-red-500 min-h-[1rem] text-sm"></div>
+              </div>
+              <div className="mt-3">
+                <input
+                  type="confirmpassword"
+                  name="confirmpassword"
+                  className="p-3 w-full outline-none border border-gray-400 focus:border-gray-600 rounded-sm
+                  focus:shadow-sm"
+                  placeholder="Confirm password"
+                />
+                <div className="mt-1 text-red-500 min-h-[1rem] text-sm"></div>
+              </div>
+              <button className="w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-500">
+                Đăng ký
+              </button>
+              <div className="mt-8 text-center">
+                <div className="flex items-center">
+                  <span className="text-sm text-slate-400">
+                    Bạn đã có tài khoản?
+                  </span>
+                  <Link className="text-red-400 text-sm ml-1" to="/login">
+                    Đăng nhập
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
