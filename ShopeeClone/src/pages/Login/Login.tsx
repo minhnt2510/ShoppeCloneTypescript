ce8adf3e67c64e19ece2cf9fef3455d6c740128f
MinhNguyen2k4 <tanminh.office183@gmail.com>
2025-07-08 11:36:01 +0700

    validate register + react hook form

--- ShopeeClone/src/pages/Login/Login.tsx
+++ ShopeeClone/src/pages/Login/Login.tsx
@@ -1,6 +1,16 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+  
   return (
     <div className="bg-orange-600">
       {/* container */}
@@ -8,7 +18,10 @@ const Login = () => {
         {/* //form  */}
         <div className="grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10">
           <div className="lg:col-span-2 lg:col-start-4">
-            <form className="p-10 rounded bg-white shadow-sm">
+            <form
+              className="p-10 rounded bg-white shadow-sm"
+              onSubmit={onSubmit}
+            >
               <div className="text-2xl">Đăng nhập</div>
               <div className="mt-8">
                 <input
@@ -32,7 +45,10 @@ const Login = () => {
                 />
                 <div className="mt-1 text-red-500 min-h-[1rem] text-sm"></div>
               </div>
-              <button className="w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-500">
+              <button
+                type="submit"
+                className="w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-500"
+              >
                 Đăng nhập
               </button>
               <div className="mt-8 text-center">
--- ShopeeClone/src/pages/Register/Register.tsx
+++ ShopeeClone/src/pages/Register/Register.tsx
@@ -1,6 +1,22 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { rules } from "../../Util/rules";
 
+interface FormData {
+  email: string;
+  password: string;
+  confirm_password: string;
+}
 const Register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = handleSubmit((data) => {
+    console.log(data);
+  });
+  console.log(errors);
   return (
     <div className="bg-orange-600">
       {/* container */}
@@ -8,41 +24,52 @@ const Register = () => {
         {/* //form  */}
         <div className="grid grid-cols-1 lg:grid-cols-5 py-12 lg:py-32 lg:pr-10">
           <div className="lg:col-span-2 lg:col-start-4">
-            <form className="p-10 rounded bg-white shadow-sm">
+            <form
+              className="p-10 rounded bg-white shadow-sm"
+              onSubmit={onSubmit}
+              noValidate
+            >
               <div className="text-2xl">Đăng ký</div>
               <div className="mt-8">
                 <input
                   type="email"
-                  name="email"
                   className="p-3 w-full outline-none border border-gray-400 focus:border-gray-600 rounded-sm
                   focus:shadow-sm"
                   placeholder="Email"
+                  {...register("email", rules.email)}
                 />
-                <div className="mt-1 text-red-500 min-h-[1rem] text-sm">
-                  Email không hợp lệ
+                <div className="mt-1 text-red-500 min-h-[1.25rem] text-sm">
+                  {errors.email?.message}
                 </div>
               </div>
               <div className="mt-3">
                 <input
                   type="password"
-                  name="password"
                   className="p-3 w-full outline-none border border-gray-400 focus:border-gray-600 rounded-sm
                   focus:shadow-sm"
                   placeholder="Password"
+                  {...register("password", rules.password)}
                 />
-                <div className="mt-1 text-red-500 min-h-[1rem] text-sm"></div>
+                <div className="mt-1 text-red-500 min-h-[1.25rem] text-sm">
+                  {errors.password?.message}
+                </div>
               </div>
               <div className="mt-3">
                 <input
-                  type="confirmpassword"
-                  name="confirmpassword"
+                  type="confirm_password"
                   className="p-3 w-full outline-none border border-gray-400 focus:border-gray-600 rounded-sm
                   focus:shadow-sm"
                   placeholder="Confirm password"
+                  {...register("confirm_password", rules.confirm_password)}
                 />
-                <div className="mt-1 text-red-500 min-h-[1rem] text-sm"></div>
+                <div className="mt-1 text-red-500 min-h-[1.25rem] text-sm">
+                  {errors.confirm_password?.message}
+                </div>
               </div>
-              <button className="w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-500">
+              <button
+                type="submit"
+                className="w-full text-center py-4 px-2 uppercase bg-red-500 text-white text-sm hover:bg-red-500"
+              >
                 Đăng ký
               </button>
               <div className="mt-8 text-center">
